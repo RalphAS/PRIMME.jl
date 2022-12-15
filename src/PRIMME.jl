ef000f60d55c5c19678c52f22cdd3a9b7823f9af
Ralph A. Smith <smith.ralph.a@gmail.com>
2022-12-15 11:08:19 -0500

    use locals for matrix and matvec

--- src/PRIMME.jl
+++ src/PRIMME.jl
@@ -27,24 +27,6 @@ function svds_initialize()
     return r
 end
 
-# matrix-vector product, y = a * x (or y = a^t * x), where
-# (void *x, PRIMME_INT *ldx, void *y, PRIMME_INT *ldy, int *blockSize,
-       # int *transpose, struct primme_svds_params *primme_svds, int *ierr);
-const _A_ = Base.Ref{Any}()
-function matrixMatvec(xp, ldxp, yp, ldyp, blockSizep, trp, parp, ierrp)
-    ldx, ldy           = unsafe_load(ldxp), unsafe_load(ldyp)
-    blockSize, tr, par = Int(unsafe_load(blockSizep)), unsafe_load(trp), unsafe_load(parp)
-    x, y = unsafe_wrap(Array, xp, (ldx, blockSize)), unsafe_wrap(Array, yp, (ldy, blockSize))
-
-    if tr == 0
-        mul!( view(y, 1:par.mLocal, :), _A_[], view(x, 1:par.nLocal, :))
-    else
-        mul!(view(y, 1:par.nLocal, :), _A_[]', view(x, 1:par.mLocal, :))
-    end
-    unsafe_store!(ierrp, 0)
-    return nothing
-end
-
 _print(r::Ref{C_params}) = ccall((:primme_display_params, libprimme), Cvoid, (C_params,), r[])
 
 _print(r::Ref{C_svds_params}) = ccall((:primme_svds_display_params, libprimme), Cvoid, (C_svds_params,), r[])
@@ -70,7 +52,7 @@ function _svds(r::Ref{C_svds_params})
     return (reshape(svecs[nConst*m .+ (1:(m*nConv))], m, nConv),
             svals,
             reshape(svecs[((nConst + nConv)*m + nConst*n) .+ (1:(n*nConv))], n, nConv),
-            err)
+            Int(err))
 end
 
 function svds(A::AbstractMatrix, k = 5;
@@ -80,15 +62,32 @@ function svds(A::AbstractMatrix, k = 5;
               method::Svds_operator = svds_op_AtA,
               maxMatvecs = 10000,
               which = :LR)
-    mul_fp = @cfunction(matrixMatvec, Cvoid,
+    # matrix-vector product, y = a * x (or y = a^t * x), where
+    # (ELT *x, PRIMME_INT *ldx, ELT *y, PRIMME_INT *ldy, int *blockSize,
+    #  int *transpose, struct primme_svds_params *primme_svds, int *ierr);
+    function mv(xp, ldxp, yp, ldyp, blockSizep, trp, parp, ierrp)
+        ldx, ldy = unsafe_load(ldxp), unsafe_load(ldyp)
+        blockSize = Int(unsafe_load(blockSizep))
+        tr, par = unsafe_load(trp), unsafe_load(parp)
+        x = unsafe_wrap(Array, xp, (ldx, blockSize))
+        y = unsafe_wrap(Array, yp, (ldy, blockSize))
+
+        if tr == 0
+            mul!( view(y, 1:par.mLocal, :), A, view(x, 1:par.nLocal, :))
+        else
+            mul!(view(y, 1:par.nLocal, :), A', view(x, 1:par.mLocal, :))
+        end
+        unsafe_store!(ierrp, 0)
+        return nothing
+    end
+    mul_fp = @cfunction($mv, Cvoid,
         (Ptr{Float64}, Ptr{Int}, Ptr{Float64}, Ptr{Int}, Ptr{Cint}, Ptr{Cint},
          Ptr{C_svds_params}, Ptr{Cint}))
     r = svds_initialize()
 
-    _A_[]            = A
     r[:m]            = size(A, 1)
     r[:n]            = size(A, 2)
-    r[:matrixMatvec] = mul_fp
+    r[:matrixMatvec] = Base.unsafe_convert(Ptr{Cvoid}, mul_fp)
     r[:numSvals]     = k
     if which in (:LR, :LM)
         r[:target] = svds_largest
@@ -108,16 +107,6 @@ function svds(A::AbstractMatrix, k = 5;
     out = _svds(r)
     free(r)
     return out
-end
-
-function matrixMatvecE(xp::Ptr{Float64}, ldxp::Ptr{PRIMME_INT}, yp::Ptr{Float64}, ldyp::Ptr{PRIMME_INT}, blockSizep::Ptr{Cint}, parp::Ptr{C_params}, ierrp::Ptr{Cint})
-    ldx, ldy           = unsafe_load(ldxp), unsafe_load(ldyp)
-    blockSize, par = Int(unsafe_load(blockSizep)), unsafe_load(parp)
-    x, y = unsafe_wrap(Array, xp, (ldx, blockSize)), unsafe_wrap(Array, yp, (ldy, blockSize))
-
-    mul!( view(y, 1:par.nLocal, :), _A_[], view(x, 1:par.nLocal, :))
-    unsafe_store!(ierrp, 0)
-    return nothing
 end
 
 function _eigs(r::Ref{C_params})
@@ -143,14 +132,25 @@ end
 
 function eigs(A::AbstractMatrix, k = 5; tol = 1e-12, maxBlockSize = 2k, verbosity::Int = 0,
               maxOuterIterations = 100, maxMatvecs = 1000, which = :LR)
-    mul_fp = @cfunction(matrixMatvecE, Cvoid,
+    function mv(xp::Ptr{Float64}, ldxp::Ptr{PRIMME_INT},
+                yp::Ptr{Float64}, ldyp::Ptr{PRIMME_INT},
+                blockSizep::Ptr{Cint}, parp::Ptr{C_params}, ierrp::Ptr{Cint})
+        ldx, ldy = unsafe_load(ldxp), unsafe_load(ldyp)
+        blockSize, par = Int(unsafe_load(blockSizep)), unsafe_load(parp)
+        x = unsafe_wrap(Array, xp, (ldx, blockSize))
+        y = unsafe_wrap(Array, yp, (ldy, blockSize))
+
+        mul!( view(y, 1:par.nLocal, :), A, view(x, 1:par.nLocal, :))
+        unsafe_store!(ierrp, 0)
+        return nothing
+    end
+    mul_fp = @cfunction($mv, Cvoid,
         (Ptr{Float64}, Ptr{Int}, Ptr{Float64}, Ptr{Int}, Ptr{Cint},
          Ptr{C_params}, Ptr{Cint}))
 
     r = eigs_initialize()
-    _A_[]            = A
     r[:n]            = size(A, 2)
-    r[:matrixMatvec] = mul_fp
+    r[:matrixMatvec] = Base.unsafe_convert(Ptr{Cvoid}, mul_fp)
     r[:numEvals]     = k
     r[:printLevel]   = verbosity
     r[:eps]          = tol
